3c68fc4d7bc62a2121eb733c042c0d793ee691b2
piyakker <ericlien86@gmail.com>
2023-01-15 21:41:49 +0800

    add morgan to inspect request

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const indexRouter = require('./routes/index');
+const morgan = require('morgan');
 
 const app = express();
 
+// middlewares
+app.use(morgan('dev'));
 app.use(express.json());
 
+// routes
 app.use('/api/v1', indexRouter)
 
 const port = 3000;
