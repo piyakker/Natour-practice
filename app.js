35c91d5b7a6b84035cb2d438eaf3e6805d0ed81d
piyakker <ericlien86@gmail.com>
2023-01-15 21:22:07 +0800

    get one tour

--- app.js
+++ app.js
@@ -38,6 +38,25 @@ app.post('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid id'
+    });
+  };
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
