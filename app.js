c3ea4dc072beae7898d63d83ef59b15532900e83
piyakker <ericlien86@gmail.com>
2023-01-15 20:05:06 +0800

    init project

---
+++ app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/tours-simple.json`)
+  );
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json(tours);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
