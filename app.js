866fb6fdac9c3aeedbf5178ae3684297b1525fd4
piyakker <ericlien86@gmail.com>
2023-01-16 17:07:32 +0800

    create appError class for operational errors & create global error handler

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const indexRouter = require('./routes/index');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const app = express();
 
 // middlewares
@@ -14,10 +17,10 @@ app.use(express.json());
 app.use('/api/v1', indexRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
-  });
-})
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
