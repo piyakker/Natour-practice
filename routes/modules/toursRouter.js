ef5dc5ef1b112bf1be1a184cc1d134ff468d5631
piyakker <ericlien86@gmail.com>
2023-01-16 12:26:45 +0800

    add top-5-cheap endpoint with alias middleware

--- routes/modules/toursRouter.js
+++ routes/modules/toursRouter.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const toursController = require('../../controllers/toursController');
 
+router.get('/top-5-cheap', toursController.aliasTopTours, toursController.getAllTours);
+
 router.get('/', toursController.getAllTours);
 router.post('/', toursController.createTour);
 router.get('/:id', toursController.getTour);
