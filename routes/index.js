6d98eba165ae05a124040f85d4e68d3af5f9fa48
piyakker <ericlien86@gmail.com>
2023-01-16 21:25:57 +0800

    add sign up function

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const toursRouter = require('./modules/toursRouter');
+const usersRouter = require('./modules/usersRouter');
 
 router.use('/tours', toursRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
