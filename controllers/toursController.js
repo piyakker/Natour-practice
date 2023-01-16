b76138131eb16819db1a46ee345376aa9768b749
piyakker <ericlien86@gmail.com>
2023-01-16 12:10:13 +0800

    add select field function to all tours

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -20,6 +20,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort(sortBy)
     }
 
+    // 4) Fielld limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     const tours = await query;
 
     res.status(200).json({
